969be636e22142a1f80acd15e0b9af8941efdc91
vizaymeena <vizaymeena@gmail.com>
2025-05-03 17:57:06 +0530

    update

--- index.js
+++ index.js
@@ -2,44 +2,42 @@
 /* 1. navbar starts */
 
 window.addEventListener('DOMContentLoaded', () => {
-    const nav = document.createElement('nav');
-    let div = document.createElement('div');
-    const ul = document.createElement('ul');
-    const li = document.createElement('li'); 
+ // Create navbar structure
+ const nav = document.createElement('nav');
+ const div = document.createElement('div');
+ const ul = document.createElement('ul');
 
-    nav.classList.add('nav');
-    div.classList.add('navDiv');
+ nav.classList.add('nav');
+ div.classList.add('navDiv');
 
-    // Website Name
-    let title = document.createElement('h1')
-    title.textContent='Road Fury'
-    title.classList.add('title')
-    div.appendChild(title)
+ // Title
+ const title = document.createElement('h1');
+ title.textContent = 'Road Fury';
+ title.classList.add('title');
+ div.appendChild(title);
 
-    li.classList.add('navLinks');
-  
-    // Loop
-    ['Home','Cars','About','Help','Register','Login'].forEach(e => {
+ // Navigation links
+ ['Home', 'Cars', 'About', 'Help', 'Register', 'Login'].forEach(text => {
+   const li = document.createElement('li');
+   li.style.listStyleType='none'
+   ul.classList.add('navLinks');
 
-    // Creating anchortag inside loop ensure anchor tag will created as per array length on which forLoop has applied
-      const a = document.createElement('a');
-      a.textContent = e;
-      a.href = '#';
-      a.classList.add('anchors');
-      li.appendChild(a); 
-    });
-  
-    ul.appendChild(li); 
-    div.appendChild(ul);
-    nav.appendChild(div);
-    const redRoad = document.createElement('div')
-    
-    nav.appendChild(redRoad)
-    
-    // prepand means appending child to the top most of the body before any elements
-    document.body.prepend(nav);
-  });
-  
+   const a = document.createElement('a');
+   a.textContent = text;
+   a.href = '#';
+   a.classList.add('anchors');
+
+   li.appendChild(a);
+   ul.appendChild(li);
+ });
+
+ div.appendChild(ul);
+ nav.appendChild(div);
+ 
+
+  // prepand means appending child to the top most of the body before any elements
+  document.body.prepend(nav)
+})
 
 // navbar ends
 
