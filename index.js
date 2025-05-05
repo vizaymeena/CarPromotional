33f905230950251d58b6df4d5792f3e93453b11e
vizaymeena <vizaymeena@gmail.com>
2025-05-05 17:48:15 +0530

    update

--- index.js
+++ index.js
@@ -156,3 +156,45 @@ window.addEventListener("DOMContentLoaded",()=>{
   displayCars(cars)
 
 })
+
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+  coverflowEffect: {
+    rotate: 50,
+    scale:1,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20
+  //   },
+  //   480: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30
+  //   },
+  //   640: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 40
+  //   }
+  // }
+});
+ 
+
