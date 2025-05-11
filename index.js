23066c90f4337914b03bad10c866cf1a84bfe4fe
vizaymeena <vizaymeena@gmail.com>
2025-05-11 09:41:33 +0530

    updaTE

--- index.js
+++ index.js
@@ -11,5 +11,28 @@ window.addEventListener('DOMContentLoaded', () => {
 		console.error("error accured at toggle arrow button", error)
 	}
 })
-
-
+var swiper = new Swiper(".coverflow_container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 2,
+  speed: 600,
+  preventClicks: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  on: {
+    click(event) {
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
+});
