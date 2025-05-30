796ff444662fb52594ece9f7ed2c68b710547cc4
vizaymeena <vizaymeena@gmail.com>
2025-05-30 23:54:51 +0530

    removed a card js

--- index.js
+++ index.js
@@ -13,42 +13,4 @@ window.addEventListener('DOMContentLoaded', () => {
 }
 )
 
-// Grid
- const cars = [
-    {
-      name: "Kia Seltos",
-      image: "./Images/kia/kia_htk.avif",
-    },
-    {
-      name: "Kia Cyrus",
-      image: "./Images/kia/kia_hte.avif",
-    },
-    {
-      name: "Kia Carnival",
-      image: "./Images/kia/kia_htk(o).jpg",
-    },
-    {
-      name:"Kia Sonet",
-      image:"./Images/kia/kia-sonet.jpg",
-    },
-    {
-      name:"Kia EV6",
-      image:"./Images/kia/kiaev6.jpg",
-    }
-  ];
 
-  let carGrid = document.getElementById("carGrid");
-
-  cars.forEach((car) => {
-    let card = document.createElement("div");
-    card.className = "car-card";
-    card.classList.add('.car-card')
-
-    card.innerHTML = `
-      <img src="${car.image}" alt="${car.name}" />
-      <h4>${car.name}</h4>
-      <p>See specification</p>
-    `;
-
-    carGrid.appendChild(card);
-  });
